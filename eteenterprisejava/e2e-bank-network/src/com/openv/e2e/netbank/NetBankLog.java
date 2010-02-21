47e922ec249001fa312d069d35cd64e703a8c89d
luoshifei@yahoo.cn <luoshifei@yahoo.cn@2ede28d4-ec4c-11de-a198-37193048d1ed>
2010-02-21 13:17:25 +0000

    初始导入

---
+++ eteenterprisejava/e2e-bank-network/src/com/openv/e2e/netbank/NetBankLog.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.openv.e2e.netbank;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 
+ * <pre>
+ * 程序的中文名称
+ * </pre>
+ * @author luoshifei  luoshifei@yahoo.cn
+ * @version 1.00.00
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容: 
+ * </pre>
+ */
+public class NetBankLog {
+
+	private static final Log log = LogFactory.getLog(NetBankLog.class);
+	
+}
